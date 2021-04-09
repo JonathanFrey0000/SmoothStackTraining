6e1353117d04f9de14813d8b99957b6303872e5a
JonathanFrey0000 <82041401+JonathanFrey0000@users.noreply.github.com>
2021-04-09 15:45:34 +0200

    added week 1 day 4 exercises

---
+++ src/Week1_day4/LineTest.java
@@ -0,0 +1,32 @@
+package Week1_day4;
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * @author  Jonathan Frey
+ * Test class for Line class
+ */
+class LineTest {
+
+    private Line line;
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+        line = new Line(1,1,2,2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSlope() {
+        assertEquals(1, line.getSlope());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getDistance() {
+        assertEquals(Math.sqrt(2), line.getDistance());
+    }
+
+    @org.junit.jupiter.api.Test
+    void parallelTo() {
+        Line line2 = new Line(2,2,3,3);
+        assertEquals(true, line.parallelTo(line2));
+
+    }
+}
